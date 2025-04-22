b814b68e62a6d34a7af06baf7aee5e69066d554e
Fardan-Nurhidayat <fardannurhidayat07.stu@pnc.ac.id>
2025-04-22 22:32:20 +0700

    Chore : exception handling in fetch api

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const config = {
   API_KEY: "6c46dbed6cmsh18c779ad73a387dp131949jsne8afd79c77ad",
-  API_HOST: "https://moviesdatabase.p.rapidapi.com/",
+  API_HOST: "https://moviesdatabase.p.rapidapi.co.id/",
 };
 
 export default config;
--- utils/fertchapi.js
+++ utils/fertchapi.js
@@ -1,14 +1,40 @@
 import config from "../config.js";
 
+// export const fetchApi = async (method, path) => {
+//   const url = `${config.API_HOST}${path}`;
+//   const response = await fetch(url, {
+//     method: method,
+//     headers: {
+//       "x-rapidapi-key": config.API_KEY,
+//       "x-rapidapi-host": "moviesdatabase.p.rapidapi.com",
+//     },
+//   });
+//   const result = await response.json();
+//   return result;
+// };
+
+/**
+ * Fetch data from API
+ * @param {string} method - HTTP method
+ * @param {string} path - API path
+ * @returns {Promise<object>} - Response data
+ */
 export const fetchApi = async (method, path) => {
-  const url = `${config.API_HOST}${path}`;
-  const response = await fetch(url, {
-    method: method,
-    headers: {
-      "x-rapidapi-key": config.API_KEY,
-      "x-rapidapi-host": "moviesdatabase.p.rapidapi.com",
-    },
-  });
-  const result = await response.json();
-  return result;
+  try {
+    const url = `${config.API_HOST}${path}`;
+    const response = await fetch(url, {
+      method: method,
+      headers: {
+        "x-rapidapi-key": config.API_KEY,
+        "x-rapidapi-host": "moviesdatabase.p.rapidapi.com",
+      },
+    });
+    if (!response.ok) {
+      throw new Error(response.statusText);
+    }
+    const result = await response.json();
+    return result;
+  } catch (error) {
+    console.error(error.message);
+  }
 };
